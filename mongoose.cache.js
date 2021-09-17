846cf389bcb29b9da179a52761e648c752e09b08
Alexander Kiryushin <kontaktAK@yandex.ru>
2021-09-17 14:44:39 +0300

    before all requests

--- mongoose.cache.js
+++ mongoose.cache.js
@@ -1,42 +1,46 @@
-import * as redis from 'redis'
-import * as util from 'util'
-import * as mongoose from 'mongoose'
-
-export function applyMongooseCache() {
-  const redisUrl = process.env.REDIS_CACHE_URL
-  const client = redis.createClient(redisUrl)
-  client.get = util.promisify(client.get)
-  const exec = mongoose.Query.prototype.exec;
-
-  mongoose.Query.prototype.exec = async function () {
-    const key = 'games_' + JSON.stringify(Object.assign({}, this.getQuery(), {
-      collection: this.mongooseCollection.name,
-      op: this.op,
-      options: this.options
-    }))
+const redis = require('redis')
+const util = require('util')
+const mongoose = require('mongoose')
 
 
-    const cacheValue = await client.get(key)
+module.exports =
+  {
+    applyMongooseCache() {
+      const redisUrl = process.env.REDIS_CACHE_URL
+      const client = redis.createClient(redisUrl)
+      client.get = util.promisify(client.get)
+      const exec = mongoose.Query.prototype.exec
 
-    if (cacheValue) {
-      const doc = JSON.parse(cacheValue)
+      mongoose.Query.prototype.exec = async function () {
+        const key = 'games_' + JSON.stringify(Object.assign({}, this.getQuery(), {
+          collection: this.mongooseCollection.name,
+          op: this.op,
+          options: this.options
+        }))
 
-      // const newValue = Array.isArray(doc) ? doc.map(d => new this.model(d)) :
-      //     ( typeof doc === 'object' ? new this.model(doc): doc)
-      // return newValue
 
-      return doc
+        const cacheValue = await client.get(key)
+
+        if ( cacheValue ) {
+          const doc = JSON.parse(cacheValue)
+
+          // const newValue = Array.isArray(doc) ? doc.map(d => new this.model(d)) :
+          //     ( typeof doc === 'object' ? new this.model(doc): doc)
+          // return newValue
+
+          return doc
+        }
+
+        const result = await exec.apply(this, arguments)
+
+        if ( result ) {
+
+          await client.set(key, JSON.stringify(result))
+        }
+
+        return result
+      }
     }
-
-    const result = await exec.apply(this, arguments)
-
-    if ( result ) {
-
-      await client.set(key, JSON.stringify(result))
-    }
-
-    return result
   }
-}
 
 
--- testing.test.js
+++ testing.test.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
-
+const { applyMongooseCache } = require('./mongoose.cache')
 describe('base test', function () {
 
-  let kitty;
-  beforeAll( () => {
+  let cats;
+  let Cat;
+  beforeAll( async () => {
     mongoose.connect('mongodb://root:root@localhost:27017/cats?authSource=admin');
-    const Cat = mongoose.model('Cat', { name: String });
-    kitty = new Cat({ name: 'Zildjian' });
 
+    Cat = mongoose.model('Cat', { name: String });
+
+    cats = ['First', 'Second', 'Third'].map(name => ({name}))
+    await Cat.deleteMany()
+    await Cat.insertMany(cats)
 
   })
   it('should correctly cache', async function () {
-    await kitty.save()
+    const cats = await Cat.find().exec()
   })
 })
